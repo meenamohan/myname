6a111b592647a18d50663d318c4e460a9ddadecb
meenamohan <peermohamed.thabib@hispark2013.in>
2014-08-13 18:01:35 +0530

    commit

---
+++ myname/main.m
@@ -0,0 +1,39 @@
+//
+//  main.m
+//  myname
+//
+//  Created by Thabib on 13/08/14.
+//  Copyright (c) 2014 peer mohamed thabib. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        //int i;
+        //for (i=1; i<=50; i++) {
+           // NSLog(@"meena");
+
+        //}
+       
+        int i=1;
+        while (i<=50) {
+            NSLog(@"meena");
+        i++;
+        }
+        
+//        int i=1;
+//
+//         do {
+//             i++;
+//
+//            NSLog(@"meena");
+//        } while (i<=50);
+        
+            }
+    return 0;
+}
+
